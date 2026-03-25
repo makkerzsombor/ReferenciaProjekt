c1f27490160ca4516b3e1877aa1514585a2a8b76
Zsombor Makker <makker.zsombor@gmail.com>
2026-03-25 09:25:39 +0100

    Created basic Navbar component

---
+++ Frontend/src/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+
+
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-800 p-4">
+            <a href="" className="text-white p-4">Tanulmányok</a>
+            <a href="" className="text-white p-4">Készségek</a>
+            <a href="" className="text-white p-4">Skills</a>
+            <a href="" className="text-white p-4">Korábbi munkahelyek</a>
+            <a href="" className="text-white p-4">Rólam</a>
+        </nav>
+    );
+};
+
+export default Navbar;
