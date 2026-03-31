9e5cabbc388c33e26384f26aeff4c426e8c666a4
makkerzsombor <makker.zsombor@gmail.com>
2026-03-31 16:53:16 +0200

    Added props to Card

--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -1,11 +1,18 @@
+type CardProps =
+    {
+        title: string;
+        description: string;
+        imgUrl:string;
+        imgAlt:string;
+    }
 
-export const Card = () =>
+export const Card = ({title, description, imgUrl, imgAlt}: CardProps) =>
 {
     return(
         <div>
-            <h1></h1>
-            <p></p>
-            <img src="" alt=""/>
+            <h1>{title}</h1>
+            <p>{description}</p>
+            <img src={imgUrl} alt={imgAlt}/>
         </div>
     )
 }
