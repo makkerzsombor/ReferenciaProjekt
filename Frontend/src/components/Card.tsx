2302bf73324321080705e4d02565405e74278d34
makkerzsombor <makker.zsombor@gmail.com>
2026-03-31 18:31:31 +0200

    Added basic design to Card component

--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -9,10 +9,12 @@ type CardProps =
 export const Card = ({title, description, imgUrl, imgAlt}: CardProps) =>
 {
     return(
-        <div>
-            <h1>{title}</h1>
-            <p>{description}</p>
-            <img src={imgUrl} alt={imgAlt}/>
+        <div className="bg-blue-100 flex items-center justify-between p-6 m-4 rounded-md shadow-xl max-w-2xl border-2 border-sky-700">
+            <div className="flex flex-col gap-2 pr-2">
+                <h1 className="text-sky-700 text-2xl font-bold">{title}</h1>
+                <p className="text-sky-700 text-base leading-relaxed">{description}</p>
+            </div>
+            <img src={imgUrl} alt={imgAlt} className="float-right m-2.5 max-w-1/5 max-w-20"/>
         </div>
     )
 }
--- Frontend/src/pages/Technologies.tsx
+++ Frontend/src/pages/Technologies.tsx
@@ -1,11 +1,14 @@
 import Navbar from "../components/Navbar.tsx";
+import {Card} from "../components/Card.tsx";
+import javaicon from "../assets/java_logo.png"
 
 export const  Technologies = () =>
 {
     return(
         <>
             <Navbar/>
-            <h1>Technológiák</h1>
+            <h1 className="m-2.5 ml-4 text-sky-700 text-2xl font-bold">Technológiák</h1>
+            <Card title="Java" description="Java frontend és backend-et is használtam már!" imgUrl={javaicon} imgAlt="Java.icon"/>
         </>
     )
 }
