276cc7c8a5cc4d9ea76a00a160161b1b285eb796
Zsombor Makker <makker.zsombor@gmail.com>
2026-04-01 09:07:36 +0200

    Added more text into java technology

--- Frontend/src/pages/Technologies.tsx
+++ Frontend/src/pages/Technologies.tsx
@@ -8,7 +8,9 @@ export const  Technologies = () =>
         <>
             <Navbar/>
             <h1 className="m-2.5 ml-4 text-sky-700 text-2xl font-bold">Technológiák</h1>
-            <Card title="Java" description="Java frontend és backend-et is használtam már!" imgUrl={javaicon} imgAlt="Java.icon"/>
+            <Card title="Java" description="Java-val 2022-ben ismerkedtem, meg amikor is java programokat írtunk technikusi évben.
+            Továbbá JavaFX felülelet hoztam létre és JavaSpring backend-el is foglalkoztam az akkori projektmunka keretein belül!"
+                  imgUrl={javaicon} imgAlt="Java.icon"/>
         </>
     )
 }
