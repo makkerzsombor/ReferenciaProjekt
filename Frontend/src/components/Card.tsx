5189956f0b950cf01c6055501469a5da00948a29
makkerzsombor <makker.zsombor@gmail.com>
2026-04-02 19:12:56 +0200

    Fixed Card tailwind bug

--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -9,7 +9,7 @@ type CardProps =
 export const Card = ({title, description, imgUrl, imgAlt}: CardProps) =>
 {
     return(
-        <div className="bg-blue-100 flex items-center justify-between p-6 m-4 rounded-md shadow-xl max-w-2xl sm:w-full border-2 border-sky-700">
+        <div className="bg-blue-100 flex items-center justify-between p-6 m-4 rounded-md shadow-xl border-2 border-sky-700">
             <div className="flex flex-col gap-2 pr-2">
                 <h1 className="text-sky-700 text-2xl font-bold">{title}</h1>
                 <p className="text-sky-700 text-base leading-relaxed">{description}</p>
--- Frontend/src/pages/Technologies.tsx
+++ Frontend/src/pages/Technologies.tsx
@@ -2,19 +2,23 @@ import Navbar from "../components/Navbar.tsx";
 import {Card} from "../components/Card.tsx";
 import javaicon from "../assets/java_logo.png"
 import cIcon from "../assets/c-logo-icon-18.png"
+import cPP from "../assets/C++-Logo.wine.png"
 
 export const Technologies = () => {
     return (
         <>
             <Navbar/>
             <h1 className="m-2.5 ml-4 text-sky-700 text-2xl font-bold">Technológiák</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 bg-red-700">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <Card title="Java" description="Java-val 2022-ben ismerkedtem, meg amikor is java programokat írtunk technikusi évben.
             Továbbá JavaFX felülelet hoztam létre és JavaSpring backend-el is foglalkoztam az akkori projektmunka keretein belül!"
                       imgUrl={javaicon} imgAlt="Java.icon"/>
                 <Card title="C#" description="C#-al 2017-ben szakközép iskolában találkoztam először. Ebben a nyelvben írtam érettségit. Továbbá az egyetemen is a
             preferált programozási nyelv. C#-ban programoztam konzol applikációkat, WPF-et, Backendet és Mauival pedig Android alkalmazást."
                       imgUrl={cIcon} imgAlt="C#.icon"/>
+                <Card title="C++" description="C++-al 2026-ban találkoztam kötelezően választható kurzus keretein belül. Egyenlőre még nem használtam sokat, de az
+                alapok itt is nagyjából ugyanazok, mint a többi nyelvben."
+                      imgUrl={cPP} imgAlt="C++.icon"/>
             </div>
         </>
     )
