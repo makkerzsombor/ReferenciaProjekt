81cf455d5e7727c6ef604a50e49a2914218f08d5
makkerzsombor <makker.zsombor@gmail.com>
2026-04-05 12:33:28 +0200

    Studies page now has proper data

--- Frontend/src/components/StudyCard.tsx
+++ Frontend/src/components/StudyCard.tsx
@@ -1,22 +1,25 @@
 type StudyProps =
     {
         schoolName: string;
-        startAndEndDate: string;
+        startDate: string;
+        endDate?: string;
         description?: string;
         achievements: string[];
     }
 
 
-export const StudyCard = ({schoolName, startAndEndDate, description, achievements}: StudyProps) => {
+export const StudyCard = ({schoolName, startDate, endDate, description, achievements}: StudyProps) => {
     return (
         <div>
             <h1>{schoolName}</h1>
             <h3>Évszámok:</h3>
-            <p>{startAndEndDate}</p>
+            <p>{startDate} - {endDate}</p>
             <p>{description}</p> {/*Ez nem kötelező*/}
+            <ul>
             {achievements.map((item, index) => (
-                <p key={index}>{item}</p>
+                <li key={index}>{item}</li>
             ))}
+            </ul>
         </div>
     )
 }
--- Frontend/src/pages/Studies.tsx
+++ Frontend/src/pages/Studies.tsx
@@ -1,11 +1,18 @@
 import Navbar from "../components/Navbar.tsx";
+import {StudyCard} from "../components/StudyCard.tsx";
 
-export const Studies = () =>
-{
-    return(
+export const Studies = () => {
+    return (
         <>
             <Navbar/>
             <h1>Tanulmányaim</h1>
+            <div>
+                <StudyCard schoolName="BMSZC Petrik Lajos Két Tanítási Nyelvű Technikum" startDate="2017" endDate="2023"
+                           description="Szak közép iskolában végeztem, ahol nyelvi előkészítő nulladik éven vettem részt. Továbbá a technikusi plusz
+                           1 évet is tettem le"
+                           achievements={["Érettségi", "Angol nyelvvizsga", "Irodai informatikus bizonyítvány",
+                               "Szoftverfejlesztő és -tesztelő informatikus oklevél", ]}/>
+            </div>
         </>
 
     )
