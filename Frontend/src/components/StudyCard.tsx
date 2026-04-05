bf351deb3dc0f55bad8e1a4ed0766d45f33c7660
makkerzsombor <makker.zsombor@gmail.com>
2026-04-05 14:04:55 +0200

    Added new design to StudyCard component and Studies page

--- Frontend/src/components/StudyCard.tsx
+++ Frontend/src/components/StudyCard.tsx
@@ -10,16 +10,29 @@ type StudyProps =
 
 export const StudyCard = ({schoolName, startDate, endDate, description, achievements}: StudyProps) => {
     return (
-        <div>
-            <h1>{schoolName}</h1>
-            <h3>Évszámok:</h3>
-            <p>{startDate} - {endDate}</p>
-            <p>{description}</p> {/*Ez nem kötelező*/}
-            <ul>
-            {achievements.map((item, index) => (
-                <li key={index}>{item}</li>
-            ))}
-            </ul>
+        <div className="relative pl-10 pb-12 last:pb-0 group">
+            <div className="absolute left-0 top-0 h-full w-0.5 bg-sky-200 group-last:h-6"></div>
+
+            <div className="absolute -left-[7px] top-1.5 h-4 w-4 rounded-full bg-white border-2 border-sky-400 transition
+             group-hover:border-sky-600 group-hover:bg-sky-600 delay-100 duration-200 shadow-sm"></div>
+
+            <div className="flex flex-col gap-2">
+                <p className="text-sm font-bold text-sky-600 bg-sky-50 px-3 py-1 rounded-xl w-fit border-1 border-sky-100">
+                    {startDate} — {endDate ? endDate : "Jelenleg"}</p>
+
+                <div className="bg-white p-8 rounded-2xl border border-slate-100 shadow-lg">
+                    <h3 className="text-2xl font-extrabold text-slate-800 uppercase">{schoolName}</h3>
+                    {/*Leírás ellenőrzés*/}
+                    {description && (<p className="mt-4 text-slate-600 text-base border-l-4 border-sky-100 pl-4">{description}</p>)}
+
+                    <div className="mt-6 flex flex-wrap gap-2">
+                        {achievements.map((item, index) => (
+                            <span key={index} className="inline-flex items-center rounded-lg bg-white px-3 py-1.5 text-sm font-medium text-sky-800 border-1
+                                border-sky-200 hover:bg-sky-50">{item}</span>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- Frontend/src/pages/Studies.tsx
+++ Frontend/src/pages/Studies.tsx
@@ -3,17 +3,43 @@ import {StudyCard} from "../components/StudyCard.tsx";
 
 export const Studies = () => {
     return (
-        <>
-            <Navbar/>
-            <h1>Tanulmányaim</h1>
-            <div>
-                <StudyCard schoolName="BMSZC Petrik Lajos Két Tanítási Nyelvű Technikum" startDate="2017" endDate="2023"
-                           description="Szak közép iskolában végeztem, ahol nyelvi előkészítő nulladik éven vettem részt. Továbbá a technikusi plusz
-                           1 évet is tettem le"
-                           achievements={["Érettségi", "Angol nyelvvizsga", "Irodai informatikus bizonyítvány",
-                               "Szoftverfejlesztő és -tesztelő informatikus oklevél", ]}/>
-            </div>
-        </>
+        <div>
+            <Navbar />
 
-    )
+            <div className="mt-12 mx-4 sm:mx-10 md:mx-20 lg:mx-24 rounded-xl">
+                <div className="flex items-center gap-4 mb-16 ml-3">
+                    <div className="h-12 w-2 bg-sky-500 rounded-xl"></div>
+                    <h1 className="text-5xl font-bold text-slate-900 tracking-tight">
+                        Tanulmányaim
+                    </h1>
+                </div>
+
+                <div className="ml-4">
+                    <StudyCard
+                        schoolName="BMSZC Petrik Lajos Két Tanítási Nyelvű Technikum"
+                        startDate="2017"
+                        endDate="2023"
+                        description="Szakközép iskolában végeztem, ahol nyelvi előkészítő nulladik éven vettem részt. Továbbá a technikusi plusz 1 évet is itt tettem le."
+                        achievements={[
+                            "Érettségi",
+                            "Angol nyelvvizsga",
+                            "Irodai informatikus bizonyítvány",
+                            "Szoftverfejlesztő és -tesztelő informatikus oklevél"
+                        ]}
+                    />
+                    <StudyCard
+                        schoolName="Óbudai Egyetem - Neumann János Informatika kar"
+                        startDate="2023"
+                        endDate=""
+                        description="Jelenleg 5. félévemet végzem az egyetemen. Tanár segédként is ügyködtem 1 félév erejéig."
+                        achievements={[
+                            "C# használata fullstack technológiáknál",
+                            "Tanár segédkezés"
+                        ]}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+
 }
