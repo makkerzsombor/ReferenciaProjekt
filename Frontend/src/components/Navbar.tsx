234b4a0419c6982e3832cc2b0a89031c0a64ecc8
makkerzsombor <makker.zsombor@gmail.com>
2026-04-06 17:22:10 +0200

    Proper mobile view for Navbar

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -5,14 +5,14 @@ import homeIcon from '../assets/home.png';
 const Navbar = () => {
     return (
         <nav className="bg-gray-800 flex flex-col md:flex-row">
-            <Link to={"/"} className="text-white px-4 py-4 hover:bg-gray-700" >
+            <Link to={"/"} className="text-white px-4 py-4 hover:bg-gray-700 flex items-center justify-center md:justify-start" >
                 <img src={homeIcon} alt="homeicon" className="max-h-6"/>
             </Link>
-            <Link to={"/Studies"} className="text-white px-4 py-4 hover:bg-gray-700">Tanulmányok</Link>
-            <Link to={"/Skills"} className="text-white px-4 py-4 hover:bg-gray-700">Készségek</Link>
-            <Link to={"/Technologies"} className="text-white px-4 py-4 hover:bg-gray-700">Technológiák</Link>
-            <Link to={"/WorkExperience"} className="text-white px-4 py-4 hover:bg-gray-700">Korábbi munkahelyek</Link>
-            <Link to={"/AboutMe"} className="text-white px-4 py-4 hover:bg-gray-700">Rólam</Link>
+            <Link to={"/Studies"} className="text-white px-4 py-4 hover:bg-gray-700 flex items-center justify-center md:justify-start">Tanulmányok</Link>
+            <Link to={"/Skills"} className="text-white px-4 py-4 hover:bg-gray-700 flex items-center justify-center md:justify-start">Készségek</Link>
+            <Link to={"/Technologies"} className="text-white px-4 py-4 hover:bg-gray-700 flex items-center justify-center md:justify-start">Technológiák</Link>
+            <Link to={"/WorkExperience"} className="text-white px-4 py-4 hover:bg-gray-700 flex items-center justify-center md:justify-start">Korábbi munkahelyek</Link>
+            <Link to={"/AboutMe"} className="text-white px-4 py-4 hover:bg-gray-700 flex items-center justify-center md:justify-start">Rólam</Link>
         </nav>
     );
 };
