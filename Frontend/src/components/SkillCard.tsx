d3072ec04dccec4776055b09842c2ace752b3ffa
makkerzsombor <makker.zsombor@gmail.com>
2026-04-25 15:03:19 +0200

    Card component refactored for SkillCard

--- Frontend/src/components/SkillCard.tsx
+++ Frontend/src/components/SkillCard.tsx
@@ -1,20 +1,19 @@
 type CardProps =
     {
-        title: string;
+        name: string;
         description: string;
-        imgUrl:string;
-        imgAlt:string;
+        expertise: number;
     }
 
-export const Card = ({title, description, imgUrl, imgAlt}: CardProps) =>
+export const SkillCard = ({name, description, expertise}: CardProps) =>
 {
     return(
         <div className="bg-blue-100 flex items-center justify-between p-6 m-4 rounded-md shadow-xl border-2 border-sky-700">
             <div className="flex flex-col gap-2 pr-2">
-                <h1 className="text-sky-700 text-2xl font-bold">{title}</h1>
+                <h1 className="text-sky-700 text-2xl font-bold">{name}</h1>
                 <p className="text-sky-700 text-base leading-relaxed">{description}</p>
+                <p className="text-sky-700 text-base leading-relaxed">{expertise}</p>
             </div>
-            <img src={imgUrl} alt={imgAlt} className="float-right m-2.5 max-w-20"/>
         </div>
     )
 }
